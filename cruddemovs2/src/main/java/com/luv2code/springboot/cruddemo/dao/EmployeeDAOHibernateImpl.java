ba675bff3945b7025b58f7da6402717f250b994d
Diana Vrote <diana.oana.vrote@gmail.com>
2022-11-10 19:01:33 +0200

    Developing DAO

--- cruddemovs2/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ cruddemovs2/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -3,8 +3,8 @@ package com.luv2code.springboot.cruddemo.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.websocket.Session;
 
+import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -27,13 +27,11 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 	@Override
 	@Transactional
 	public List<Employee> findAll() {
-		
 		//get the current hibernate session
 		Session currentSession = entityManager.unwrap(Session.class);
 		
 		// create a query
-		Query<Employee> theQuery = 
-				currentSession.createQuery("from Employee", Employee.class);
+		Query<Employee> theQuery = currentSession.createQuery("FROM Employee", Employee.class);
 		
 		//execute query and get the result list
 		List<Employee> employees = theQuery.getResultList();
